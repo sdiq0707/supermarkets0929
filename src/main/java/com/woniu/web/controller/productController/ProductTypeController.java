6b27a01b7a3bef8a067e224c8da49af33007c6c3
zhang <Administrator@PC-20160408QFGU.lan>
2019-09-17 14:43:31 +0800

    ProductTypeController上传

---
+++ src/main/java/com/woniu/web/controller/productController/ProductTypeController.java
@@ -0,0 +1,48 @@
+package com.woniu.web.controller.productController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.woniu.domain.Producttype;
+import com.woniu.service.IProducttypeService;
+
+@Controller
+@RequestMapping("producttypes")
+public class ProductTypeController {
+	@Autowired
+	   private IProducttypeService service;
+	@PostMapping
+	@ResponseBody()
+	public void save(Producttype producttype) {
+		service.save(producttype);
+	}
+	@DeleteMapping
+	@ResponseBody
+	public void delete(Integer Ptypeid) {
+		service.delete(Ptypeid);
+	}
+	@PutMapping
+	@ResponseBody
+	public void update(Producttype producttype) {
+		service.update(producttype);
+	}
+	@GetMapping
+	@ResponseBody
+	public List<Producttype> findAll(){
+		return service.select();
+	}
+	@GetMapping(value="{ptypeid}")
+	@ResponseBody
+	public Producttype findOne(@PathVariable Integer ptypeid) {
+		return service.select(ptypeid);
+	}
+}
