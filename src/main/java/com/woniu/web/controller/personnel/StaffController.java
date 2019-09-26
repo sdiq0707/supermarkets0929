46f61a68979535392681738d46f1086f0dd6f6f4
chenxiaoyu <666@qq.com>
2019-09-26 15:28:02 +0800

    会员、员工界面美化

--- src/main/java/com/woniu/web/controller/personnel/StaffController.java
+++ src/main/java/com/woniu/web/controller/personnel/StaffController.java
@@ -1,5 +1,6 @@
 package com.woniu.web.controller.personnel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,9 +43,11 @@ public class StaffController {
 	public List<Staff> findAll(){
 		return service.findAll();
 	}
-	@GetMapping(value="{staffid}")
+	@GetMapping(value = "{staffid}")
 	@ResponseBody
 	public Staff findOne(@PathVariable Integer staffid) {
+		System.out.println(11111111);
+		List<Staff> list=new ArrayList<>();
 		return service.find(staffid);
 	}
 
