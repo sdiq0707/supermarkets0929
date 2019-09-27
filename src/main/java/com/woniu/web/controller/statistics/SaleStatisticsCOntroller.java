ce3ca10949c21eccb9f83ff3f2c8420ad8da756e
yangfan <1096884498@qq.com>
2019-09-27 16:41:36 +0800

    转admin

--- src/main/java/com/woniu/web/controller/statistics/SaleStatisticsCOntroller.java
+++ src/main/java/com/woniu/web/controller/statistics/SaleStatisticsCOntroller.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.woniu.domain.Triple;
 import com.woniu.service.ISalesDetailService;
+import com.woniu.service.ISalesService;
 
 @Controller
 @RequestMapping("saleStatistics")
@@ -21,9 +22,48 @@ public class SaleStatisticsCOntroller {
 	@Autowired
 	private ISalesDetailService service;
 	
+	@Autowired
+	private ISalesService salesService;
+	
 	@PostMapping("high")
 	@ResponseBody
 	public Map high() {
+		List<String> list = new ArrayList<>();
+		List<Long> list2 = new ArrayList<>();
+		List<Double> list3 = new ArrayList<>();
+		List<Object> list4 = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+		Map<String, Object> map2 = new HashMap<>();
+		Map<String, Object> map3 = new HashMap<>();
+		List<Triple<String, BigDecimal, Double>> detailList = service.findAllBySales();
+		for (Triple<String, BigDecimal, Double> triple : detailList) {
+			System.out.println(triple);
+			list.add(triple.getA());
+			list2.add(triple.getB().longValue());
+			list3.add(triple.getC());
+		}
+		
+		map2.put("name", "销售总金额");
+		map2.put("type", "column");
+		map2.put("yAxis", 1);
+		map2.put("data", list3);
+		
+		map3.put("name", "销售总件数");
+		map3.put("type", "spline");
+		map3.put("yAxis", 0);
+		map3.put("data", list2);
+		
+		list4.add(map2);
+		list4.add(map3);
+		
+		map.put("categories", list);
+		map.put("series", list4);
+		return map;
+	}
+	
+	@PostMapping("high2")
+	@ResponseBody
+	public Map high2() {
 		List<String> list = new ArrayList<>();
 		List<Long> list2 = new ArrayList<>();
 		List<Double> list3 = new ArrayList<>();
