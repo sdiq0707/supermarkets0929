a00dce039e15d7688269dbabae37c3bbfea00f5a
yangfan <1096884498@qq.com>
2019-09-29 15:03:55 +0800

    111

--- src/main/java/com/woniu/web/controller/TlogController.java
+++ src/main/java/com/woniu/web/controller/TlogController.java
@@ -1,52 +1,5 @@
 package com.woniu.web.controller;
 
-<<<<<<< HEAD
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.woniu.domain.Tlog;
-import com.woniu.service.ITlogService;
-
-@RestController		//@ResponseBody的作用：将目标方法的返回值自动转换成json格式，然后返回给前端
-@RequestMapping("/logs")
-public class TlogController {
-	
-	@Autowired
-	private ITlogService service;
-	
-	@PostMapping("save")
-	public String save(Tlog tlog) {
-		service.save(tlog);
-		return null;
-	}
-	@DeleteMapping("delete")
-	public String delete(Integer logid) {
-		service.delete(logid);
-		return "删除成功";
-	}
-	@PutMapping("update")
-	public String update(Tlog tlog) {
-		service.update(tlog);
-		return "修改成功";
-	}
-	@GetMapping("findOne")
-	public Tlog findOne(Integer logid) {
-		return service.findOne(logid);
-	}
-	@GetMapping("findAll")
-	public List<Tlog> findAll() {
-		return service.findAll();
-	}
-
-}
-=======
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -115,4 +68,4 @@ public class TlogController {
 	}
 
 }
->>>>>>> branch 'master' of git@github.com:sdiq0707/supermarkets.git
+
